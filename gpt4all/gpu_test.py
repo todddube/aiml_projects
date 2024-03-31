aed5090a98ab629107c755ba8546a124d604b45a
Todd Dube <todddube@users.noreply.github.com>
2024-03-31 12:34:59 -0400

    working

--- gpt4all/gpu_test.py
+++ gpt4all/gpu_test.py
@@ -1,8 +1,7 @@
 from gpt4all import GPT4All 
 
+# device='amd', device='intel'
 model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", device='gpu') 
-
-# device='amd', device='intel' 
-
+ 
 output = model.generate("The capital of France is ", max_tokens=3) 
 print(output)
