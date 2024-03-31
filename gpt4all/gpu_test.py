0056f77a7f668fb2a6db80c55201679899d203c2
Todd Dube <todddube@users.noreply.github.com>
2024-03-31 15:51:19 -0400

    working

--- gpt4all/gpu_test.py
+++ gpt4all/gpu_test.py
@@ -1,7 +1,7 @@
-from gpt4all import GPT4All 
-
-# device='amd', device='intel'
-model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", device='gpu') 
- 
-output = model.generate("The capital of France is ", max_tokens=3) 
-print(output)
+from gpt4all import GPT4All
+model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", device='gpu')
+tokens = []
+with model.chat_session():
+    for token in model.generate("What is the capital of France?", streaming=True):
+        tokens.append(token)
+print(tokens)
