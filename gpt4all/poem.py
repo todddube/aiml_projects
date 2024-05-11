c52b1f38eb42377f39faf493c9601ec4d9af1ede
Todd Dube <todddube@users.noreply.github.com>
2024-05-11 10:33:11 -0400

    ss

--- gpt4all/poem.py
+++ gpt4all/poem.py
@@ -7,5 +7,3 @@ with model.chat_session():
     response2 = model.generate(prompt='write me a short poem', temp=0)
     response3 = model.generate(prompt='thank you', temp=0)
     print(model.current_chat_session)
-    
-    
